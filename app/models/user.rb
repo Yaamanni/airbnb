bec25c8f713024d000047a8717abc38d852a5ef1
yaamanni kasavan <yaamanni19@gmail.com>
2022-08-15 21:20:15 +0800

    updated wishlist controller and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+<<<<<<< HEAD
   has_many :bookings, dependent: :destroy
   has_many :listings, dependent: :destroy
   # has_many :wishlists
+=======
+  has_one :wishlist
+>>>>>>> updated wishlist controller and models
 end
