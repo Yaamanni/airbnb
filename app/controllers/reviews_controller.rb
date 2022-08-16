95036604af80bb01c24ec32402c2c0920bdb71b4
kyla wong <kylawong1@hotmail.com>
2022-08-16 13:29:57 +0800

    fixed linter complains

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -58,13 +58,14 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def review_params
-      params.require(:review).permit(:content, :rating)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
