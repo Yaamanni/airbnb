6b24cc411e5abef9d0c87a6c5a3f59a8dab169cf
Jie Lin Lee <lin.leejie@gmail.com>
2022-08-19 13:56:41 +0800

    adjusted z-index

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :booking
   belongs_to :listing
 
-  # validates :rating, numericality: { greater_than: 0 }, presence: true
+  validates :rating, numericality: { greater_than: 0 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   has_many :bookings, dependent: :destroy
   has_many :listings, dependent: :destroy
-  # has_many :wishlists
 
   has_one :wishlist
 end
